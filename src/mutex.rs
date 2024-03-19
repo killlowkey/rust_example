84f2aa3df62b281c3ac8a40ed86f9eb3e92bce6f
StarBus <xzc2860072080@gmail.com>
2024-03-19 16:49:21 +0800

    feat: 添加 thread example

--- src/mutex.rs
+++ src/mutex.rs
@@ -1,12 +1,11 @@
 // https://kaisery.github.io/trpl-zh-cn/ch16-03-shared-state.html
 #[cfg(test)]
 mod tests {
-    use std::rc::Rc;
-    use std::sync::{Arc, Mutex};
+    use std::sync::{Arc, mpsc, Mutex, RwLock};
     use std::thread;
 
     #[test]
-    fn test_basic() {
+    fn test_mutex() {
         let m = Mutex::new(5);
 
         {
@@ -16,6 +15,50 @@ mod tests {
 
         println!("m = {:?}", m);
     }
+
+    #[test]
+    fn test_rw_lock() {
+        let lock = Arc::new(RwLock::new(5));
+        {
+            let x = lock.read().unwrap(); // 离开作用域才会释放锁
+            println!("x before write = {}", x);
+        } // read lock 释放
+        {
+            let mut w = lock.write().unwrap();
+            *w += 1;
+            println!("Write value: {}", *w);
+        } // 写锁释放
+    }
+
+    // 多线程 lock 需要使用 Arc 进行引用
+    #[test]
+    fn test_rw_lock2() {
+        let mut handles = vec![];
+        let lock = Arc::new(RwLock::new(5));
+
+        for _ in 0..5 {
+            let mut lock_clone = Arc::clone(&lock);
+            let handle = thread::spawn(move || {
+                {
+                    let r1 = lock_clone.read().unwrap(); // Acquire a read lock
+                    println!("Read value: {}", *r1); // Safe read without data race
+                } // Read lock is automatically released here
+                {
+                    let mut w = lock_clone.write().unwrap(); // Acquire a write lock
+                    *w += 1; // Modify the data safely
+                    println!("Write value: {}", *w); // Safe write without data race
+                } // Write lock is automatically released here
+            });
+            handles.push(handle);
+        }
+
+        // Wait for all threads to complete
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        println!("Value after threads modified it: {}", *lock.read().unwrap());
+    }
+
 
     #[test]
     fn test_multi_thread() {
@@ -43,5 +86,40 @@ mod tests {
         }
 
         println!("Result: {}", *counter.lock().unwrap());
+    }
+
+    // https://doc.rust-lang.org/std/sync/mpsc/fn.channel.html
+    #[test]
+    fn test_channel() {
+        // Create a new channel
+        let (tx, rx) = mpsc::channel();
+        // Spawn a new thread and move the sender into it
+        thread::spawn(move || {
+            // Send a message on the channel
+            tx.send("Hello from the spawned thread!").unwrap();
+        });
+        // Receive the message in the main thread
+        let received = rx.recv().unwrap();
+        println!("Received: {}", received);
+    }
+
+    // https://doc.rust-lang.org/std/sync/mpsc/
+    #[test]
+    fn test_multithreading_channel() {
+        // Create a new channel
+        let (tx, rx) = mpsc::channel();
+        for i in 0..5 {
+            let sender = tx.clone();
+            // Spawn a new thread and move the sender into it
+            let handler = thread::spawn(move || {
+                // Send a message on the channel
+                sender.send(format!("Hello from the spawned thread! {}", i)).unwrap();
+            });
+        }
+
+        for i in 0..5 {
+            let received = rx.recv().unwrap();
+            println!("Received: {}", received);
+        }
     }
 }
